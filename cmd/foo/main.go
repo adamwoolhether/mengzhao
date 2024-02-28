a45d68d2fe23f10e3e37a101943903073f16364e
adamwoolhether <adamwoolhether@gmail.com>
2024-02-29 01:06:15 +0800

    replicate stable diffusion pt 1 1544

---
+++ cmd/foo/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/replicate/replicate-go"
+)
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal(err)
+	}
+
+	ctx := context.Background()
+
+	// You can also provide a token directly with
+	// `replicate.NewClient(replicate.WithToken("r8_..."))`
+	r8, err := replicate.NewClient(replicate.WithTokenFromEnv())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// https://replicate.com/stability-ai/stable-diffusion
+	version := "stability-ai/stable-diffusion:ac732df83cea7fff18b8472768c88ad041fa750ff7682a21affe81863cbe77e4"
+
+	input := replicate.PredictionInput{
+		"prompt": "an astronaut riding a horse on mars, hd, dramatic lighting",
+	}
+
+	webhook := replicate.Webhook{
+		URL:    os.Getenv("WEBHOOK_URL"),
+		Events: []replicate.WebhookEventType{"start", "completed"},
+	}
+
+	// Run a model and wait for its output
+	output, err := r8.Run(ctx, version, input, &webhook)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("output: ", output)
+
+	//// Create a prediction
+	version = "ac732df83cea7fff18b8472768c88ad041fa750ff7682a21affe81863cbe77e4"
+	prediction, err := r8.CreatePrediction(ctx, version, input, &webhook, false)
+	if err != nil {
+		log.Fatal(err)
+	}
+	//
+	// Wait for the prediction to finish
+	err = r8.Wait(ctx, prediction)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("output: ", prediction)
+}
--- go.mod
+++ go.mod
@@ -11,6 +11,7 @@ require (
 	github.com/joho/godotenv v1.5.1
 	github.com/lib/pq v1.10.9
 	github.com/nedpals/supabase-go v0.4.0
+	github.com/replicate/replicate-go v0.17.0
 	github.com/uptrace/bun v1.1.17
 	github.com/uptrace/bun/dialect/pgdialect v1.1.17
 	github.com/uptrace/bun/extra/bundebug v1.1.17
@@ -30,5 +31,6 @@ require (
 	github.com/vmihailenco/msgpack/v5 v5.4.1 // indirect
 	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
 	go.uber.org/atomic v1.10.0 // indirect
-	golang.org/x/sys v0.16.0 // indirect
+	golang.org/x/sync v0.6.0 // indirect
+	golang.org/x/sys v0.17.0 // indirect
 )
