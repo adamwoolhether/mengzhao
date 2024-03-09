4342353ec01439ffb725877a5346e3b0e5088ad1
adamwoolhether <adamwoolhether@gmail.com>
2024-03-09 14:34:11 +0800

    run script css fix

--- run.sh
+++ run.sh
@@ -44,20 +44,16 @@ printf "ngrok URL:\t%s\n" "$PUBLIC_URL"
 
 export WEBHOOK_URL=$PUBLIC_URL/replicate/callback
 
-npx tailwindcss -i view/css/app.css -o public/styles.css --watch &
+wgo -file=.go -file=.templ -file=.js -xfile=_templ.go npx tailwindcss -i view/css/app.css -o public/styles.css :: go run -tags dev . &
 PID1=$!
-printf "tailwindcss\tPID: %s\n" $PID1
-
-wgo -file=.go -file=.templ -file=.js -file=.css -xfile=_templ.go go run -tags dev . &
-PID2=$!
-printf "wgo\t\tPID: %s\n" $PID2
+printf "wgo\t\tPID: %s\n" $PID1
 
 templ generate --watch --proxy=http://localhost:42069 &
 PID3=$!
 printf "templ\t\tPID: %s\n" $PID3
 
 cleanup() {
-  for pid in $PID3 $PID2 $PID1 $PID0; do
+  for pid in $PID3 $PID1 $PID0; do
     if kill -0 $pid 2>/dev/null; then
       printf "PID\t\t%s stopping...\n" $pid
       kill $pid || printf "PID\t\t%s not stopped\n" $pid
@@ -72,6 +68,6 @@ cleanup() {
 
 trap cleanup SIGINT
 
-wait $PID3 $PID2 $PID1 $PID0
+wait $PID3 $PID1 $PID0
 
 osascript -e 'tell application "Google Chrome" to close (tabs of window 1 whose URL contains "http://127.0.0.1:7331/")'
